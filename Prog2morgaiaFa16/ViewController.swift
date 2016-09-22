fa68f1f8571ca3301ad2a1126ad27b3b7e88ca0f
Bradley Morgan <bradley@eightbitapps.com>
2016-09-21 23:51:17 -0500

    added input validation and calculation

--- Prog2morgaiaFa16/ViewController.swift
+++ Prog2morgaiaFa16/ViewController.swift
@@ -9,6 +9,8 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // outlets for capturing and manipulating text field input
 
     @IBOutlet weak var tfLoanAmount: UITextField!
     @IBOutlet weak var tfNumPayments: UITextField!
@@ -17,31 +19,95 @@ class ViewController: UIViewController {
     @IBOutlet weak var lblPaymentAmount: UILabel!
     
     override func viewDidLoad() {
+    
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
+    // main payment calculation function
     
     @IBAction func calculatePayment(sender: AnyObject) {
     
-    // P = ( r * A ) / ( 1 - (1+r)-N)
-        let interestRate = Double(tfInterestRate.text!)!
-        let loanAmount = Double(tfLoanAmount.text!)!
-        let numPayments = Int(tfNumPayments.text!)!
+        let interestRate = Float(tfInterestRate.text!)
+        let loanAmount = Double(tfLoanAmount.text!)
+        let numPayments = Int(tfNumPayments.text!)
+        var balloonPayment = Double(tfBalloonPayment.text!)
         
-        let paymentAmount = Double((interestRate * loanAmount) / (1 - pow((1 + interestRate),(Double(numPayments) * -1))))
+        // input validation
+        
+        if(numPayments == nil ||  numPayments! < 0) {
+            
+            presentViewController(createAlert("The number of payments must be a positive integer."), animated: true, completion: nil)
+            
+            return
+            
+        }
+        
+        if(interestRate == nil || interestRate < 0 || interestRate > 100) {
+            
+            presentViewController(createAlert("The interest rate must be a value between 0 and 100."), animated: true, completion: nil)
+            
+            return
+            
+        }
+        
+        if(loanAmount == nil || loanAmount < 0) {
+            
+            presentViewController(createAlert("Invalid loan amount."), animated: true, completion: nil)
+            
+            return
+            
+        }
+        
+        if(balloonPayment < 0 || balloonPayment > loanAmount) {
+            
+            presentViewController(createAlert("Invalid balloon payment amount."), animated: true, completion: nil)
+            
+            return
+            
+        } else if(balloonPayment == nil) {
+            
+            balloonPayment = 0.0
+            
+        }
+        
+        // if all input is valid, perform the payment calculation
+    
+        let interestRatePercentage = interestRate! / 100 // provided interest rate is 0 - 100 
+        
+        let netLoanAmount = Float(loanAmount!) - (Float(balloonPayment!) / pow((1 + interestRatePercentage), Float(numPayments!))) // calculate the loan amount taking into account the balloon payment
+        
+        let paymentAmount = ((interestRatePercentage * Float(netLoanAmount)) / (1 - pow((1 + interestRatePercentage),(Float(numPayments!) * -1))))
+        
+        //  display the result
         
         lblPaymentAmount.text = String(paymentAmount);
         
-        self.view.resignFirstResponder()
         
     }
     
-
+    
+    // function for abstracting validation alerts
+    
+    func createAlert(message: String) -> UIAlertController {
+        
+        let inputAlert:UIAlertController = UIAlertController(title: "Input Error", message: message, preferredStyle: UIAlertControllerStyle.Alert)
+        
+        let inputAlertAction:UIAlertAction = UIAlertAction(title: "Okay", style: UIAlertActionStyle.Cancel, handler: nil)
+        
+        inputAlert.addAction(inputAlertAction)
+        
+        return inputAlert
+        
+    }
+    
+    // gesture recognizer action to hide keyboard on tap
+    
+    @IBAction func hideNumberPad(sender: AnyObject) {
+        
+        self.view.endEditing(true)
+        
+    }
 
 }
 
